abd6b974c6371789be7a2c9906b8956a9cea26ee
dmitrymack <dmitrymack12@gmail.com>
2022-03-08 13:50:40 +0700

    Added command of fill database

---
+++ work/management/commands/FillTable.py
@@ -0,0 +1,36 @@
+from django.core.management import BaseCommand
+import work.models as mdl
+import work.data as data
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        mdl.Company.objects.all().delete()
+        mdl.Specialty.objects.all().delete()
+        mdl.Vacancy.objects.all().delete()
+
+        for company in data.companies:
+            mdl.Company.objects.create(
+                name=company['title'],
+                location=company['location'],
+                logo=company['logo'],
+                description=company['description'],
+                employee_count=company['employee_count']
+            )
+        for spec in data.specialties:
+            mdl.Specialty.objects.create(
+                code=spec['code'],
+                title=spec['title']
+            )
+        for vac in data.jobs:
+            mdl.Vacancy.objects.create(
+                title=vac['title'],
+                speciality=mdl.Specialty.objects.get(code=vac['specialty']),
+                company=mdl.Company.objects.get(id=int(vac['company'])),
+                skills=vac['skills'],
+                description=vac['description'],
+                salary_min=vac['salary_from'],
+                salary_max=vac['salary_to'],
+                posted=vac['posted']
+            )
