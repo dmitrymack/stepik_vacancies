a94502a331f5aff5eaf6285449b18125bcbdc88d
dmitrymack <dmitrymack12@gmail.com>
2022-03-08 12:43:51 +0700

    Create models and migrations

--- work/models.py
+++ work/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class Company(models.Model):
+    name = models.CharField(max_length=50)
+    location = models.CharField(max_length=50)
+    logo = models.URLField(default='https://place-hold.it/100x60')
+    description = models.TextField()
+    employee_count = models.IntegerField()
+
+
+class Specialty(models.Model):
+    code = models.CharField(max_length=50)
+    title = models.CharField(max_length=50)
+    picture = models.URLField(default='https://place-hold.it/100x60')
+
+
+class Vacancy(models.Model):
+    title = models.CharField(max_length=50)
+    speciality = models.ForeignKey(Specialty, on_delete=models.CASCADE, related_name='vacancies')
+    company = models.ForeignKey(Company, on_delete=models.CASCADE, related_name='vacancies')
+    skills = models.CharField(max_length=255)
+    description = models.TextField()
+    salary_min = models.IntegerField()
+    salary_max = models.IntegerField()
+    posted = models.DateField()
+
--- work/views.py
+++ work/views.py
@@ -1,4 +1,6 @@
+from django.http import HttpResponseNotFound, HttpResponseServerError
 from django.shortcuts import render
+
 
 def main_view(request):
     return render(request, "work/index.html")
@@ -15,5 +17,14 @@ def vacancy_view(request, id):
 def list_vacancies_view(request):
     return render(request, "work/vacancies.html")
 
+
 def vacancy_cat_view(request, category):
     return render(request, "work/vacancy_cat.html")
+
+
+def custom404(request, exception):
+    return HttpResponseNotFound('<h1>Ошибка 404:</h1><h2>Данной страницы не существует</h2>')
+
+
+def custom500(request):
+    return HttpResponseServerError('<h1>Ошибка 500:</h1><h2>К сожалению, наш сервер сломался</h2>')
